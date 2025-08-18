bf1faefe606f9c5a51b69f1136cb0f548bd9edc8
hunter-volkman <huntervolkman@protonmail.com>
2025-08-17 23:11:45 -0400

    Fix entrypoint

--- cmd/module/main.go
+++ cmd/module/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	sensorreplay "sensor-replay"
 
+	"github.com/hunter-volkman/sensor-replay"
 	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/module"
 )
 
 func main() {
-	err := module.Main(sensor.API, sensorreplay.Model)
+	err := module.AddModularResource(sensor.API, sensorreplay.Model)
 	if err != nil {
 		panic(err)
 	}
+	module.ModularMain()
 }
